e23beb53a83963d766597330f8edbb6edda2cca3
rohit <rohit@gmail>
2025-04-01 17:15:56 +0530

    test1 from test1 branch

--- app/Http/Controllers/GitWebhookController.php
+++ app/Http/Controllers/GitWebhookController.php
@@ -11,7 +11,7 @@ class GitWebhookController extends Controller
 {
     public function handlePush(Request $request)
     {
-        //test12312
+        //test branch
         // Validate GitHub webhook signature (optional but recommended for security)
         $val = $this->validateGitHubSignature($request);
         logger('response'.$val);
